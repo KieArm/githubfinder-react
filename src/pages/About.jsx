7e234d787b5018e26a6902b1de4eb0b9e1aa974d
KieArm <87212808+KieArm@users.noreply.github.com>
2022-05-19 13:30:14 +0800

    Update About.jsx
    Updated descriptions
    Fixed spelling error.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,16 +3,18 @@ function About() {
     <div>
       <h1 className='text-4xl mb-8'>Github Finder - React</h1>
       <p className='mb-2'>
-        This SPA allows the user to search GitHub profiles, view details and explore reposoitories.
+        This Single Page Application allows the browser to search GitHub profiles, view user details
+        and explore repositories.
       </p>
       <p className='mb-2'>
-        Built with React v18, React Router and Axios. Styled with TailwindCSS and DaisyUI.
+        Built with React v18, React Router, Context API and Axios. Styled with TailwindCSS and
+        DaisyUI.
       </p>
       <p className='mt-8'>
         Version: <span className='text-secondary'>1.1.0</span>
       </p>
       <p className='mt-2'>
-        Updated: <span className='text-secondary'>12th May 2022</span>
+        Updated: <span className='text-secondary'>19th May 2022</span>
       </p>
       <p className='mt-2'>
         By: <span className='text-secondary'>Kieran Armbrecht</span>
